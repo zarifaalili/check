e7e046226230ae2560f525dc78e81d3d6dc19c5f
zarifaalili <alilizarifa@gmail.com>
2025-02-25 13:18:20 +0400

    commit 3

---
+++ src/Zarifa.java
@@ -0,0 +1,21 @@
+public class Zarifa {
+
+    int age;
+    String job;
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getJob() {
+        return job;
+    }
+
+    public void setJob(String job) {
+        this.job = job;
+    }
+}
